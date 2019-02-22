d54796a1a6bd63b2eb5fdba3ca175089bfc9b52f
TheBuyna <thebuyna@gmail.com>
2019-02-22 11:44:41 +0100

    Express en nodemon

--- _BACK_/server.js
+++ _BACK_/server.js
@@ -1,12 +1,6 @@
-//var http = require('http');
 const X = require('express');
 const APP = X();
 const PORT = 1997;
-
-//HTTP.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/html'});
-//  res.end('Hello World!');
-//}).listen(1999);
 
 APP.use('/',X.static(__dirname + '/_SITE_/'));
 
